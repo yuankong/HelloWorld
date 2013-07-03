6b9352d11dcb7f65e8ba949fc15ce8d0f76a62c5
Yuankong <xinglvliang@163.com>
2013-07-03 12:54:04 +0800

    The seconde time to sumbit.

---
+++ proForGitTest/src/com/Test.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Test {
+	public static void main(String[] args){
+		System.out.println("Just for test!");
+		System.out.println("Just for test!2");
+		System.out.println("Just for test!3");
+		
+	}
+
+}
